1c44d868119d420e74c12e8283983ef3f6083d8e
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-04-07 05:50:21 -0400

    Validando dados do usuario antes de entra na rota

--- perguntas/src/boot/beforeEach.js
+++ perguntas/src/boot/beforeEach.js
@@ -1,12 +1,25 @@
 import { FirebaseApp } from 'boot/firebase'
+import { Axios } from 'boot/axios'
 
-export default ({ router, store, Vue }) => {
+export default async ({ router, store, Vue }) => {
+  
+  // const getUser = async (uid) => {
+  //   let res = await Axios.get(`users/${uid}.json`)
+  //   let { data } = res;
+  //   store.dispatch("auth/setUser", data);
+  // }
+  
   router.beforeEach((to, from, next) => {
     FirebaseApp.auth().onAuthStateChanged((user) => {      
       if(user){
-        store.dispatch("auth/setUser", user);
+        store.dispatch("auth/setUserUid", user.uid);
+        Axios.get(`users/${user.uid}.json`).then(response => {
+            store.dispatch("auth/setUser", response.data);
+            next()
+        })
+      }else{
+        next()
       }
-      next()
     });
   })
 }
